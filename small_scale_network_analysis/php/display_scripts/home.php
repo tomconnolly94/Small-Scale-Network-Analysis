5e7e6c50132fcd10189c842ed4081bb1aaa6af67
Tom Connolly <tom.connolly2511@gmail.com>
2016-11-10 13:28:46 +0000

    changes

--- small_scale_network_analysis/php/display_scripts/home.php
+++ small_scale_network_analysis/php/display_scripts/home.php
@@ -18,7 +18,11 @@
             </div>
 
             <div id="user_welcome">
-                <p style="width:auto;height:auto;display:table">Welcome <a href=""><?php echo $_SESSION["name"]; ?></a></p>
+                <p style="width:auto;height:auto;display:table">Welcome <a href=""><?php if(!isset($_SESSION["name"])){
+    $_SESSION["logged_in"]=0;
+    echo "yo";
+    header("Locaton:  login.php");
+}else{ echo $_SESSION["name"]; }?></a></p>
             </div>
         </div>
 
@@ -30,9 +34,6 @@
                 <a href=""><li style="background-image:url(../../images/Vulnerability.png)"><div class="button">Vulnerabilities</div></li></a>
             </ol>
         </div>
-
         <?php include("../fragments/footer.php"); ?>
-
     </body>
-
 </html>
