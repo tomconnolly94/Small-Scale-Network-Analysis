cdb5211f25bdfc22dde8fb78e3ce4c376841d4a4
Tom Connolly <tom.connolly2511@gmail.com>
2016-12-09 20:49:02 +0000

    Changes

--- small_scale_network_analysis/php/display_scripts/mapping.php
+++ small_scale_network_analysis/php/display_scripts/mapping.php
@@ -3,13 +3,12 @@
     <head>
         <title>Network Mapping</title>
         <!-- javascript references -->
-        <script src="../../dependencies/three.min.js"></script>
+        <script src="../../dependencies/pixi.min.js"></script>
         <script src="../../dependencies/jquery-3.1.1.min.js"></script>
-        <script src="../../dependencies/controls_lib/OrbitControls.js"></script>
-        <script src="../../javascript/mapping/init.js"></script>
-        <script src="../../javascript/mapping/animate.js"></script>
-        <script src="../../javascript/mapping/data_request.js"></script>
-        <script src="../../javascript/general_purpose_funcs.js"></script>
+        <script src="../../javascript/3js/mapping/init.js"></script>
+        <script src="../../javascript/3js/mapping/animate.js"></script>
+        <script src="../../javascript/3js/mapping/data_request.js"></script>
+        <script src="../../javascript/3js/general_purpose_funcs.js"></script>
         <!--css references -->
         <link rel="stylesheet" type="text/css" href="../../css/mapping.css">
         <!-- include references made available to all pages -->
@@ -36,13 +35,42 @@
         
         <div id="javascript">
             <script>
+                /* 3js init and run
                 //create globals for 3js scene
                 var scene, camera, renderer;
                 this.master_data;
 
                 get_data();
                 
-                document.onload = animate(); 
+                document.onload = animate();
+                */ 
+                
+                //Create the renderer
+                var renderer = PIXI.autoDetectRenderer(256, 256);
+                //Add the canvas to the HTML document
+                document.body.appendChild(renderer.view);
+                //Create a container object called the `stage`
+                var stage = new PIXI.Container();
+
+                
+                PIXI.loader
+                  .add("../../images/attack.jpg")
+                  .load(setup);
+
+                function setup() {
+                  var sprite = new PIXI.Sprite(
+                    PIXI.loader.resources["../../images/attack.jpg"].texture
+                  );
+                    
+                    stage.addChild(sprite);
+                
+                
+                    //Tell the `renderer` to `render` the `stage`
+                    renderer.render(stage);
+                    }
+                
+                
+                
             </script>
         </div>
         
