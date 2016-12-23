d1138f9336ca97408af8cbd212f2eefd5266b207
Tom Connolly <tom.connolly2511@gmail.com>
2016-12-23 22:21:38 +0000

    changes

--- small_scale_network_analysis/php/display_scripts/mapping.php
+++ small_scale_network_analysis/php/display_scripts/mapping.php
@@ -1,83 +1,51 @@
 <html>
 
-    <head>
-        <title>Network Mapping</title>
-        <!-- javascript references -->
-        <script src="../../dependencies/pixi.min.js"></script>
-        <script src="../../dependencies/jquery-3.1.1.min.js"></script>
-        <script src="../../javascript/3js/mapping/init.js"></script>
-        <script src="../../javascript/3js/mapping/animate.js"></script>
-        <script src="../../javascript/3js/mapping/data_request.js"></script>
-        <script src="../../javascript/3js/general_purpose_funcs.js"></script>
-        <!--css references -->
-        <link rel="stylesheet" type="text/css" href="../../css/mapping.css">
-        <!-- include references made available to all pages -->
-        <?php include("../fragments/generic_head_refs.php"); ?>
-    </head>
+<head>
+    <title>Network Mapping</title>
+    <!-- javascript references -->
+    <script src="../../dependencies/pixi.min.js"></script>
+    <script src="../../dependencies/jquery-3.1.1.min.js"></script>
+    <script src="../../javascript/pixi/mapping/init.js"></script>
+    <script src="../../javascript/pixi/mapping/animate.js"></script>
+    <script src="../../javascript/pixi/mapping/mapping_gen_purp_funcs.js"></script>
+    <script src="../../javascript/pixi/mapping/click_listeners.js"></script>
+    <script src="../../javascript/pixi/master_gen_purp_funcs.js"></script>
+    <!--css references -->
+    <link rel="stylesheet" type="text/css" href="../../css/mapping.css">
+    <!-- include references made available to all pages -->
+    <?php include("../fragments/generic_head_refs.php"); ?>
+</head>
 
-    
-    <body>
-        <!--include fragment scripts -->
-        <?php
-            include("../fragments/check_logged_in.php");
-        ?>
-        <?php
-            include("../fragments/generic_page_header.php");
-        ?>
-
-        <!-- main body of content -->
-        
-        
-        <div id="container">
-            <h3 id="title" class="centered_items"> Network Mapping </h3>
-            <div id="txtHint"> </div>
-        </div>
-        
-        <div id="javascript">
-            <script>
-                /* 3js init and run
-                //create globals for 3js scene
-                var scene, camera, renderer;
-                this.master_data;
-
-                get_data();
-                
-                document.onload = animate();
-                */ 
-                
-                //Create the renderer
-                var renderer = PIXI.autoDetectRenderer(256, 256);
-                //Add the canvas to the HTML document
-                document.body.appendChild(renderer.view);
-                //Create a container object called the `stage`
-                var stage = new PIXI.Container();
-
-                
-                PIXI.loader
-                  .add("../../images/attack.jpg")
-                  .load(setup);
-
-                function setup() {
-                  var sprite = new PIXI.Sprite(
-                    PIXI.loader.resources["../../images/attack.jpg"].texture
-                  );
-                    
-                    stage.addChild(sprite);
-                
-                
-                    //Tell the `renderer` to `render` the `stage`
-                    renderer.render(stage);
-                    }
-                
-                
-                
-            </script>
-        </div>
-        
-        <!--include footer -->
-        <?php include("../fragments/footer.php"); ?>
-        
-    </body>
-
+<body>
+    <!--include fragment scripts -->
+    <?php
+        include("../fragments/check_logged_in.php");
+    ?>
+    <?php
+        include("../fragments/generic_page_header.php");
+    ?>
+    <!-- main body of content -->
+    <div id="container">
+        <h3 id="title" class="centered_items"> Network Mapping </h3>
+        <div id="txtHint"> </div>
+    </div>
+    <div id="javascript">
+        <script>
+            //script to init global vars
+            var stage; //master container for pixi scene
+            var renderer; //pixi renderer
+            var nodes = []; //collection of node graphics
+            var data = []; //collection of data received from the server
+            var lines = new Array(); //collection of lines connecting nodes
+            var centre_node; //centre node of the scene
+            var new_data = false; //boolean to indicate that new data has arrived at the client
+            var use_server_events = false; //boolean to indicate which type of data transmission will be used
+            //star the pixi scene
+            PIXI.loader.add("../../images/attack.jpg").load(init);
+        </script>
+    </div>
+    <!--include footer -->
+    <?php include("../fragments/footer.php"); ?>
+</body>
 
 </html>
--- small_scale_network_analysis/php/processing_scripts/ajax_response/get_mapping_data.php
+++ small_scale_network_analysis/php/processing_scripts/ajax_response/get_mapping_data.php
@@ -1,11 +1,21 @@
 <?php
+   
 
-$first = array("hostname" => "lin12345", "ip_address" => "111.111.111.111");
-$second = array("hostname" => "mac12345", "ip_address" => "222.222.222.222");
-$third = array("hostname" => "win12345", "ip_address" => "333.333.333.333");
+    $first = array("hostname" => "lin12345", "ip_address" => "111.111.111.111");
+    $second = array("hostname" => "mac12345", "ip_address" => "222.222.222.222");
+    $third = array("hostname" => "win12345", "ip_address" => "333.333.333.333");
+    $fourth = array("hostname" => "lin11122", "ip_address" => "444.444.444.444");
+    $fifth = array("hostname" => "lin11122", "ip_address" => "444.444.444.444");
+    $sixth = array("hostname" => "win12345", "ip_address" => "333.333.333.333");
+    $seventh = array("hostname" => "lin11122", "ip_address" => "444.444.444.444");
+    $eigth = array("hostname" => "lin11122", "ip_address" => "444.444.444.444");
+    $ninth = array("hostname" => "lin11122", "ip_address" => "444.444.444.444");
 
-$return = array(0 => $first, 1 => $second, 2 => $third);
 
-header('Content-Type: application/json');
-echo json_encode($return);
+
+    $return = array(0 => $first, 1 => $second, 2 => $third, 3 => $fourth, 4 => $fifth, 5 => $sixth, 6 => $seventh, 7 => $eigth, 8 => $ninth);
+
+
+    //echo json_encode($return);
+
 ?>
